be0356710c9a142d750118ee71b777564a9185b2
Shefin-Sir <96075281+Shefin-Sir@users.noreply.github.com>
2021-12-14 09:47:46 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -8,23 +8,15 @@ const Lang = Language.getString('gitlink');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
-    var respoimage = await axios.get(config.LIZA, { responseType: 'arraybuffer' })
+    var respoimage = await axios.get(config.KELLY, { responseType: 'arraybuffer' })
 
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 *owner number wa.me/919567489404*
-
-
 *whatsapp group : https://chat.whatsapp.com/HtNneOrmY8W1U3gkP4pXOh*
-
-
-*githublink       https://github.com/Shefin-Sir/Kelly-Mwol*
-
-
+*githublink       _https://tinyurl.com/mvhbn86k_*
 *audio commads    _https://tinyurl.com/ydvlpbol_*
-
-
 *sticker commads  _https://tinyurl.com/yfpd23yn_*     
 `}) 
 
