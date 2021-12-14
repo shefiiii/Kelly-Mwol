8d095260035c59cdbd95dee7b4cd112fb50f6875
Shefin-Sir <96075281+Shefin-Sir@users.noreply.github.com>
2021-12-14 10:01:20 +0530

    Update and rename kellymwolv2.js to lizamwolv2.js

--- plugins/lizamwolv2.js
+++ plugins/lizamwolv2.js
@@ -1,4 +1,4 @@
-const KellyMwol = require('../events');
+const LizaMwol = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
